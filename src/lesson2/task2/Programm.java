56b1fe8db89e2521a73b4a9a35344c2fdd031d0d
JustSenoko <just.senoko@gmail.com>
2020-02-20 22:10:25 +0300

    initial

---
+++ src/lesson2/task2/Programm.java
@@ -0,0 +1,9 @@
+package lesson2.task2;
+
+class Programm{
+    public static void main(String[] args){
+        Report report = new Report();
+        report.calculate();
+        report.output();
+    }
+}
---
+++ src/lesson2/task2/ReportItem.java
@@ -0,0 +1,18 @@
+package lesson2.task2;
+
+class ReportItem{
+    private String description;
+
+    private float amount;
+
+    public ReportItem(String description, float amount){
+        this.description = description;
+        this.amount = amount;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public float getAmount(){
+        return amount;
+    }
+}
